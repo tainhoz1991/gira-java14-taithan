0f04746224d4ee71a12655a35ea97d174bc77cfe
tainhoz1991 <taithan.nguyen88@gmail.com>
2021-12-12 09:48:02 +0700

    Initial commit

---
+++ src/main/java/Cybersoft/javabackend/girajava14taithan/GiraJava14TaithanApplication.java
@@ -0,0 +1,13 @@
+package Cybersoft.javabackend.girajava14taithan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraJava14TaithanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraJava14TaithanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cybersoft/javabackend/girajava14taithan/GiraJava14TaithanApplicationTests.java
@@ -0,0 +1,13 @@
+package Cybersoft.javabackend.girajava14taithan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraJava14TaithanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
